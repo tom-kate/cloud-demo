5a3fa0218ee484e8b7ffeac16c713021b032049d
baikaixu <biakaixu1@163.com>
2021-01-05 21:03:40 +0800

    order payment作为client整合进Eureka

--- cloud-consumer-order80/src/main/java/com/tomkate/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/tomkate/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.tomkate.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Tom
  * @version 1.0
  * @date 2021/1/5 17:57
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderMain80 {
     public static void main(String[] args) {
--- cloud-provider-payment8001/src/main/java/com/tomkate/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/tomkate/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.tomkate.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Tom
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2021/1/5 15:36
  */
 
+@EnableEurekaClient
 @SpringBootApplication
 public class PaymentMain8001 {
     public static void main(String[] args) {
