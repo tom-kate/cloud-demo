fd2800aa201ead64450df8f058e3b338e301cc87
baikaixu1@163.com <baikaixu1@163.com>
2021-01-08 16:03:29 +0800

    springCloud Stream 生产者搭建 注：检查好yml文件缩进！！！！！！！！！！

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/tomkate/springcloud/StreamMQMain8801.java
@@ -0,0 +1,17 @@
+package com.tomkate.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Tom
+ * @version 1.0
+ * @date 2021/1/8 14:40
+ */
+@SpringBootApplication
+public class StreamMQMain8801 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8801.class, args);
+    }
+}
